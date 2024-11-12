fc5296212d3c3e0ed663880c59b8dae22ef0791d
Jaime Gonzalez Carbajo <jgoncarb@emeal.nttdata.com>
2024-11-12 14:49:38 +0100

    Initial commit

---
+++ src/main/java/com/jaime/finance_springboot_app/FinanceSpringbootAppApplication.java
@@ -0,0 +1,13 @@
+package com.jaime.finance_springboot_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceSpringbootAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceSpringbootAppApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jaime/finance_springboot_app/controller/FinanceRestController.java
@@ -0,0 +1,8 @@
+package com.jaime.finance_springboot_app.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FinanceRestController {
+
+}
