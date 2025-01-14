154ef545e34751841d3a58b0b393e22072324ff4
Jaime-GC <jgcarbajo1@gmail.com>
2025-01-15 00:39:45 +0100

    Tests de servicios completados

--- src/test/java/com/jaime/finance_springboot_app/controllers/ExpenseControllerTests.java
+++ src/test/java/com/jaime/finance_springboot_app/controllers/ExpenseControllerTests.java
@@ -21,7 +21,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import com.jaime.finance_springboot_app.controllers.ExpenseController;
 import com.jaime.finance_springboot_app.models.Category;
 import com.jaime.finance_springboot_app.models.Expense;
 import com.jaime.finance_springboot_app.models.User;
